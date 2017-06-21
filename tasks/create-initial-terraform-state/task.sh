c9b8f9ffd38f5c151f40fa5f09a6bdf535024e59
Saurabh Gupta <saurabhgupta.sg@gmail.com>
2017-06-21 19:23:59 -0400

    add default config

--- tasks/create-initial-terraform-state/task.sh
+++ tasks/create-initial-terraform-state/task.sh
@@ -21,8 +21,8 @@ mkdir -p ~/.aws
 touch ~/.aws/credentials
 
 chmod 600 ~/.aws/credentials
-
-echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" > ~/.aws/credentials
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
 echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
 
 cat ~/.aws/credentials
