10025dc197d14bab8f40476b657c2469826a2efa
Saurabh Gupta <saurabhgupta.sg@gmail.com>
2017-06-21 19:19:53 -0400

    fix credential file creation

--- tasks/create-initial-terraform-state/task.sh
+++ tasks/create-initial-terraform-state/task.sh
@@ -19,9 +19,8 @@ set -ex
 
 mkdir -p ~/.aws
 touch ~/.aws/credentials
-echo "" > ~/.aws/credentials
 
-echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> ~/.aws/credentials
+echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" > ~/.aws/credentials
 echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
 
 files=$(aws --endpoint-url $S3_ENDPOINT --region $S3_REGION s3 ls "${S3_BUCKET_TERRAFORM}/")
